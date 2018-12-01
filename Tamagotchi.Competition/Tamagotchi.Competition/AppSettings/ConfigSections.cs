d9bedfa64bbb5af807f429d2fc7fb2ca72534b1b
Piotr Palic <petr.palic@altenar.com>
2018-12-01 17:09:39 +0300

    prepare working env

--- Tamagotchi.Competition/Tamagotchi.Competition/AppSettings/ConfigSections.cs
+++ Tamagotchi.Competition/Tamagotchi.Competition/AppSettings/ConfigSections.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿
 namespace Tamagotchi.Competition.AppSettings
 {
-    public class ConfigSections
+    public static class ConfigSections
     {
+        public const string LOGGING = "Logging";
     }
 }
