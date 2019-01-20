51eaf883d41fd3b74e8f6dfb2ac1ca72c1cb5f25
Piotr Palic <skipper_tm@outlook.com>
2019-01-20 23:28:43 +0300

    add new migration with default data

--- Tamagotchi.Competition/Tamagotchi.Competition/Context/TamagotchiCompetitionContext.cs
+++ Tamagotchi.Competition/Tamagotchi.Competition/Context/TamagotchiCompetitionContext.cs
@@ -62,6 +62,187 @@ namespace Tamagotchi.Competition.Context
 
                 entity.Property(e => e.Value).HasColumnName("VALUE");
             });
+
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 1,
+                ActionCode = "brush",
+                RoomCode = "playRoom",
+                DeseaseCode = null,
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 5
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 2,
+                ActionCode = "takePicture",
+                RoomCode = "playRoom",
+                DeseaseCode = null,
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 10
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 3,
+                ActionCode = "playBall",
+                RoomCode = "playRoom",
+                DeseaseCode = null,
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 15
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 4,
+                ActionCode = "playTicTacToe",
+                RoomCode = "playRoom",
+                DeseaseCode = null,
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 15
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 5,
+                ActionCode = "giveAntiviral",
+                RoomCode = "hospital",
+                DeseaseCode = "cold",
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 100
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 6,
+                ActionCode = "pshikIngalipt",
+                RoomCode = "hospital",
+                DeseaseCode = "angina",
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 100
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 7,
+                ActionCode = "stripCandy",
+                RoomCode = "hospital",
+                DeseaseCode = "inflammationTricks",
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 100
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 8,
+                ActionCode = "giveAntihistamine",
+                RoomCode = "hospital",
+                DeseaseCode = "allergy",
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 100
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 9,
+                ActionCode = "dumplings",
+                RoomCode = "kitchen",
+                DeseaseCode = null,
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 50
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 10,
+                ActionCode = "pizza",
+                RoomCode = "kitchen",
+                DeseaseCode = null,
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 45
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 11,
+                ActionCode = "paste",
+                RoomCode = "kitchen",
+                DeseaseCode = null,
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 30
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 12,
+                ActionCode = "soup",
+                RoomCode = "kitchen",
+                DeseaseCode = null,
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 25
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 13,
+                ActionCode = "porridge",
+                RoomCode = "kitchen",
+                DeseaseCode = null,
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 20
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 14,
+                ActionCode = "yogurt",
+                RoomCode = "kitchen",
+                DeseaseCode = null,
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 15
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 15,
+                ActionCode = "oatCookies",
+                RoomCode = "kitchen",
+                DeseaseCode = null,
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 2
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 16,
+                ActionCode = "apple",
+                RoomCode = "kitchen",
+                DeseaseCode = null,
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 5
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 17,
+                ActionCode = "water",
+                RoomCode = "kitchen",
+                DeseaseCode = null,
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 3
+            });
+            modelBuilder.Entity<Events>().HasData(new Events()
+            {
+                Id = 18,
+                ActionCode = "tea",
+                RoomCode = "kitchen",
+                DeseaseCode = null,
+                Start = new System.TimeSpan(0, 0, 0),
+                Finish = new System.TimeSpan(23, 0, 0),
+                Value = 3
+            });
         }
     }
 }
